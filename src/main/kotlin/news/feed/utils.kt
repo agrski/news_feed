1056959269e733a52b20c9bab8200a1bfdeca43f
Alex Rakowski <alex.g.rakowski@gmail.com>
2020-05-25 22:28:08 +0100

    Rename variable to better convey intent

--- src/main/kotlin/news/feed/utils.kt
+++ src/main/kotlin/news/feed/utils.kt
@@ -6,7 +6,7 @@ import org.slf4j.event.Level
 fun Route.log(msg: String, level: Level = Level.INFO) {
     val logger = this.application.environment.log
 
-    val levelLogger: (String) -> Unit = when(level) {
+    val logAtLevelFor: (String) -> Unit = when(level) {
         Level.TRACE -> logger::debug
         Level.DEBUG -> logger::debug
         Level.INFO  -> logger::info
@@ -14,5 +14,5 @@ fun Route.log(msg: String, level: Level = Level.INFO) {
         Level.ERROR -> logger::error
     }
 
-    levelLogger(msg)
+    logAtLevelFor(msg)
 }
