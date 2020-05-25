55e1aafb182638dc15906c18c29a4b1893941243
Alex Rakowski <alex.g.rakowski@gmail.com>
2020-05-25 20:12:21 +0100

    Add log ext. fun. for Route contexts
    Calling `(call.)application.environment.log.<level>` is verbose and
    more difficult to read than simply `log()`.

---
+++ src/main/kotlin/news/feed/utils.kt
@@ -0,0 +1,18 @@
+package news.feed
+
+import io.ktor.routing.*
+import org.slf4j.event.Level
+
+fun Route.log(msg: String, level: Level = Level.INFO) {
+    val logger = this.application.environment.log
+
+    val levelLogger: (String) -> Unit = when(level) {
+        Level.TRACE -> logger::debug
+        Level.DEBUG -> logger::debug
+        Level.INFO  -> logger::info
+        Level.WARN  -> logger::warn
+        Level.ERROR -> logger::error
+    }
+
+    levelLogger(msg)
+}
